3b49cd4de2661abb808524a122edd6036958efad
Scott Baker <bakers@snikt.web-ster.com>
2019-01-07 07:59:40 -0800

    Put all the functions from include/* in the main .ino file

--- CrawlSpaceMonitor.ino
+++ CrawlSpaceMonitor.ino
@@ -1,14 +1,19 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <OneWire.h>
-#include "include/misc.ino"
 #include <dht11.h>
-#include "include/dht11.ino"
+
+int   get_ds_temp(byte pin, char sensor_id[][25], float* sensor_value);
+int   init_dht11(int pin, dht11 *obj);
+float get_dht11_humidty(dht11 obj);
+char  *get_dht11_temp_string(dht11 obj, char *ret);
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(10, 1, 1, 160);
+
+dht11 DHT11; // Glotal DHT11 object
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
@@ -187,5 +192,144 @@ void loop() {
 		// close the connection:
 		client.stop();
 		Serial.println("client disconnected");
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+int addr_to_str(byte addr[8], char* addr_str) {
+  // Sensors have an 8 byte address, we can save memory if we only use the first 4
+  int id_len = 4; // Either 4 or 8
+
+  // Should addresses include the ":" separator
+  byte include_colon = 1;
+
+  // Init the return string
+  strcpy(addr_str,"");
+
+  for (int i = 0; i < id_len; i++) {
+    char tmp_segment[5] = "";
+    sprintf(tmp_segment,"%02x", addr[i]);
+
+    /*
+    char t[40] = "";
+    sprintf(t,"byte #%i is %i = %s\n",i,addr[i],tmp_segment);
+    Serial.print(t);
+    */
+
+    // If we're including colons, and it's NOT the last octet
+    if ((include_colon) && i < (id_len - 1)) {
+      strcat(tmp_segment,":");
+    }
+
+    // Append this segment to the return string
+    strcat(addr_str,tmp_segment);
+  }
+
+  return id_len;
+}
+
+float c_to_f(float tempC) {
+  float tempF = (tempC * 9 / 5) + 32;
+
+  return tempF;
+}
+
+int get_ds_temp(byte pin, char sensor_id[][25], float* sensor_value) {
+  OneWire ds(pin);
+
+  byte data[12];
+  byte addr[8];
+  byte found = 0;
+
+  while (ds.search(addr)) {
+
+    if (OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return -1000;
+    }
+
+    if (addr[0] != 0x10 && addr[0] != 0x28) {
+      Serial.print("Device is not recognized");
+      return -1001;
+    }
+
+    found++;
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0xBE); // Read Scratchpad
+
+    for (byte i = 0; i < 9; i++) { // we need 9 bytes
+      data[i] = ds.read();
+    }
+
+    byte MSB = data[1];
+    byte LSB = data[0];
+
+    float tempRead = ((MSB << 8) | LSB); //using two's compliment
+    float TemperatureSum = tempRead / 16;
+
+    char addr_str[25] = "";
+    addr_to_str(addr, addr_str);
+
+    /*
+    char t[50] = "";
+    sprintf(t,"Sensor #%i = %s\n",found,addr_str);
+    Serial.print(t);
+    */
+
+    //sensor_id[found - 1] = addr_str;
+    strcpy(sensor_id[found - 1], addr_str);
+    sensor_value[found - 1] = c_to_f(TemperatureSum);
+  }
+
+  ds.reset_search();
+
+  return found;
+}
+
+////////////////////////////////////////////////////////////////////
+// DHT11 stuff
+////////////////////////////////////////////////////////////////////
+
+float get_dht11_humidty(dht11 obj) {
+  return (int)obj.humidity;
+}
+
+float get_dht11_temp(dht11 obj) {
+  return c_to_f(obj.temperature);
+}
+
+char *get_dht11_temp_string(dht11 obj, char *ret) {
+	float temperature = c_to_f(obj.temperature);
+
+	// Init ret to be 100% sure
+	strcpy(ret,"");
+
+	// Convert the float to a left aligned string
+	dtostrf(temperature, -6, 2, ret);
+
+	// Remove any trailing whitespace
+	for (byte i = 0; i < strlen(ret); i++) {
+		if (ret[i] == ' ') {
+			ret[i] = 0;
+		}
+	}
+
+	return ret;
+}
+
+int init_dht11(int pin, dht11 *obj) {
+	int check = obj->read(pin); // Has to run before you get temp
+
+	if (check == 0) {
+		return 1;
+	} else {
+		return 0;
 	}
 }
